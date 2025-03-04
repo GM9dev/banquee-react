4bcd892884a52fb666809d07f7516e6d172795b6
Gesse Medeiros <gessemedeiros@Gesses-MacBook-Air.local>
2025-03-04 21:16:38 +1300

    Upgrade new migration - site_peoples

---
+++ backends/migrations/migrations/20250304075817_site_perfects.js
@@ -0,0 +1,33 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable("site_perfects", function (table) {
+    table.increments();
+
+    table.string("sub_title");
+
+    table.string("main_title");
+
+    table.string("main_title_2");
+
+    table.string("description");
+
+    table.string("description_2");
+
+    table.string("main_button_text");
+
+    table.string("second_button_text");
+
+    table.timestamps(true, true);
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists("site_perfects");
+};
---
+++ backends/migrations/migrations/20250304081117_site_peoples.js
@@ -0,0 +1,27 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable("site_peoples", function (table) {
+    table.increments();
+
+    table.string("sub_title");
+
+    table.string("main_title");
+
+    table.string("info");
+
+    table.string("boxes");
+
+    table.timestamps(true, true);
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists("site_peoples");
+};
