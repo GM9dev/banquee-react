36b2cc403c3e3c9566fda1de41730c5d532ba427
Gesse Medeiros <gessemedeiros@Gesses-MacBook-Air.local>
2025-03-04 21:35:34 +1300

    Upgrade new migration - site_blogs

---
+++ backends/migrations/migrations/20250304083303_site_blogs.js
@@ -0,0 +1,25 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable("site_blogs", function (table) {
+    table.increments();
+
+    table.string("title");
+
+    table.string("options");
+
+    table.string("boxes");
+
+    table.timestamps(true, true);
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists("site_blogs");
+};
