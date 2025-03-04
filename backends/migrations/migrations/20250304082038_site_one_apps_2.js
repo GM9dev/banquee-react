c39c8a51790b292276f3aa1f374897d35898680a
Gesse Medeiros <gessemedeiros@Gesses-MacBook-Air.local>
2025-03-04 21:24:05 +1300

    Upgrade new migration - site_one_apps_2

---
+++ backends/migrations/migrations/20250304082038_site_one_apps_2.js
@@ -0,0 +1,25 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable("site_one_apps_2", function (table) {
+    table.increments();
+
+    table.string("main_title");
+
+    table.string("description");
+
+    table.string("list_benefits");
+
+    table.timestamps(true, true);
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists("site_one_apps_2");
+};
